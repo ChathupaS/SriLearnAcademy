3d21f568d1a6675f0c6dd53f27fcb1633593d5e9
ChathupaS <chathupasandeepd@gmail.com>
2026-05-17 18:11:57 +0530

    hero

--- client/src/components/student/NavBar.tsx
+++ client/src/components/student/NavBar.tsx
@@ -10,7 +10,7 @@ const NavBar = () => {
 
   return (
     <div
-      className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-white py-4 ${isCourseListPage ? "bg-white" : "bg-cyan-100/70"}`}
+      className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 py-4 ${isCourseListPage ? "bg-white" : "bg-cyan-100/70"}`}
     >
       <img
         src={assets.logo}
