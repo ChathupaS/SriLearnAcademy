5e61bd4801987ca994e6ef759c8e741030856291
ChathupaS <chathupasandeepd@gmail.com>
2026-05-29 00:04:24 +0530

    coursedetails intro

--- client/src/components/student/CourseCard.tsx
+++ client/src/components/student/CourseCard.tsx
@@ -26,7 +26,7 @@ const CourseCard = ({ course }: CourseCardProps) => {
       />
       <div className="p-3 text-left">
         <h3 className="text-base font-semibold">{course.courseTitle}</h3>
-        <p className="text-gray-500">{course.educator}</p>
+        <p className="text-gray-500">SriLearn Academy</p>
         <div className="flex items-center space-x-2">
           <p>{calculateRating(course)}</p>
           <div className="flex">
--- client/src/pages/student/CourseDetails.tsx
+++ client/src/pages/student/CourseDetails.tsx
@@ -1,12 +1,13 @@
 import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { AppContext } from "../../context/AppContext";
-import type { Course } from "../../assets/assets";
+import { assets, type Course } from "../../assets/assets";
+import Loading from "../../components/student/Loading";
 
 const CourseDetails = () => {
   const { id } = useParams<{ id: string }>();
   const [courseData, setCourseData] = useState<Course | null>(null);
-  const { allCourses } = useContext(AppContext);
+  const { allCourses, calculateRating } = useContext(AppContext);
 
   const fetchCourseData = async () => {
     const findCourse = allCourses.find((course) => course._id === id) ?? null;
@@ -17,14 +18,58 @@ const CourseDetails = () => {
     fetchCourseData();
   }, []);
 
-  return (
-    <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-betweenndLpx-36 px-8 md:pt-30 pt-20 text-left">
-      <div className="absolute top-0 left-0 w-full h-section-height bg-linear-to-b from-cyan-100/70"></div>
-      {/* left column */}
-      <div>hi</div>
-      {/* right column */}
-      <div>bye</div>
-    </div>
+  return courseData ? (
+    <>
+      <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-20 pt-20 text-left">
+        <div className="absolute top-0 left-0 w-full h-section-height bg-linear-to-b from-cyan-100/70"></div>
+        {/* left column */}
+        <div className="max-w-xl z-10 text-gray-500">
+          <h1 className="md:text-course-details-heading-large text-course-details-heading-small font-semibold text-gray-800">
+            {courseData?.courseTitle}
+          </h1>
+          <p
+            className="pt-4 md:text-base text-sm"
+            dangerouslySetInnerHTML={{
+              __html: courseData.courseDescription.slice(0, 200),
+            }}
+          ></p>
+          {/* review and ratings */}
+          <div className="flex items-center space-x-2 pt-3 pb-1 text-sm">
+            <p>{calculateRating(courseData)}</p>
+            <div className="flex">
+              {[...Array(5)].map((_, i) => (
+                <img
+                  key={i}
+                  src={
+                    i < Math.floor(calculateRating(courseData))
+                      ? assets.star
+                      : assets.star_blank
+                  }
+                  alt=""
+                  className="w-3.5 h-3.5"
+                />
+              ))}
+            </div>
+            <p className="text-blue-600">
+              ( {courseData.courseRatings.length}{" "}
+              {courseData.courseRatings.length > 1 ? "ratings" : "rating"})
+            </p>
+            <p>
+              {courseData.enrolledStudents.length}{" "}
+              {courseData.enrolledStudents.length > 1 ? "students" : "student"}
+            </p>
+          </div>
+          <p className="text-sm">
+            Course by{" "}
+            <span className="text-blue-600 underline">SriLearn Academy</span>
+          </p>
+        </div>
+        {/* right column */}
+        <div>bye</div>
+      </div>
+    </>
+  ) : (
+    <Loading />
   );
 };
 
