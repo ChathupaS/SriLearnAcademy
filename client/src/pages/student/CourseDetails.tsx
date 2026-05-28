967e61f09e4b392c95fc414f60a421dc2352312d
ChathupaS <chathupasandeepd@gmail.com>
2026-05-28 23:32:18 +0530

    coursedetails css

--- client/src/pages/student/CourseDetails.tsx
+++ client/src/pages/student/CourseDetails.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../../context/AppContext";
+import type { Course } from "../../assets/assets";
 
 const CourseDetails = () => {
+  const { id } = useParams<{ id: string }>();
+  const [courseData, setCourseData] = useState<Course | null>(null);
+  const { allCourses } = useContext(AppContext);
+
+  const fetchCourseData = async () => {
+    const findCourse = allCourses.find((course) => course._id === id) ?? null;
+    setCourseData(findCourse);
+  };
+
+  useEffect(() => {
+    fetchCourseData();
+  }, []);
+
   return (
-    <div>
-      <h1>CourseDetails Page</h1>
+    <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-betweenndLpx-36 px-8 md:pt-30 pt-20 text-left">
+      <div className="absolute top-0 left-0 w-full h-section-height bg-linear-to-b from-cyan-100/70"></div>
+      {/* left column */}
+      <div>hi</div>
+      {/* right column */}
+      <div>bye</div>
     </div>
   );
 };
