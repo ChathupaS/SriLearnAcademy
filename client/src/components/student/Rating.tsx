63a9bdd055d69de1a7b79c6de59deee09ff8bc45
ChathupaS <chathupasandeepd@gmail.com>
2026-06-02 00:16:18 +0530

    student completed

--- client/src/components/student/Rating.tsx
+++ client/src/components/student/Rating.tsx
@@ -1,9 +1,40 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
-const Rating = () => {
+interface RatingProps {
+  initialRating?: number;
+  onRate?: (value: number) => void;
+}
+
+const Rating = ({ initialRating = 0, onRate }: RatingProps) => {
+  const [rating, setRating] = useState<number>(initialRating);
+
+  const handleRating = (value: number) => {
+    setRating(value);
+    if (onRate) onRate(value);
+  };
+
+  useEffect(() => {
+    if (initialRating) {
+      setRating(initialRating);
+    }
+  }, [initialRating]);
+
   return (
     <div>
-      <h1>Rating</h1>
+      {Array.from({ length: 5 }, (_, index) => {
+        const starValue = index + 1;
+        return (
+          <span
+            key={index}
+            className={`text-xl sm:text-2xl cursor-pointer transition-colors ${
+              starValue <= rating ? "text-yellow-500" : "text-gray-400"
+            }`}
+            onClick={() => handleRating(starValue)}
+          >
+            &#9733;
+          </span>
+        );
+      })}
     </div>
   );
 };
